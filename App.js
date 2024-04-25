acb4cb7ae726bac177f79e5a95b5765c2d0d6807
unknown <abanoob.meseha@gmail.com>
2024-04-25 14:38:19 +0200

    Added: APP Home Screen

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Image, TouchableOpacity } from 'react-native';
+import COLORS from './src/constants/colors';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to GOLDHUB</Text>
+      <Image style={{width:'80%', height:'30%'}} source={require('./assets/images/LOGO.png')}></Image>
+      <Text style={styles.welcomeText}> أهلا بك بـــعـــالـــم الــــذهـــــــب </Text>
+      <TouchableOpacity style={styles.primaryBTN}>
+        <Text style={styles.btnP_Text}> ابـــدأ الان </Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.seconderyBTN}>
+        <Text style={styles.btnS_Text}> لدي حساب بالفعل </Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,49 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: COLORS.darkBlue,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  welcomeText:{
+    color:COLORS.Gold,
+    fontSize:60,
+    fontWeight:'bold',
+    textShadowColor:'black',
+    textAlign:'center',
+    marginTop:'10%'
+  },
+  primaryBTN:{
+    width:"70%",
+    height:70,
+    borderRadius:20,
+    backgroundColor:COLORS.gray,
+    marginTop:30,
+    paddingVertical:'1%',
+    borderColor:COLORS.Gold,
+    borderWidth:3,
+  },
+  btnP_Text:{
+    color:COLORS.Gold,
+    fontSize:40,
+    fontWeight:'bold',
+    textAlign:'center'
+  },
+  seconderyBTN:{
+    width:"70%",
+    height:70,
+    borderRadius:20,
+    backgroundColor:COLORS.Gold,
+    paddingVertical:'2%',
+    borderColor:COLORS.gray,
+    borderWidth:3,
+    marginTop:15,
+
+  },
+  btnS_Text:{
+    color:COLORS.gray,
+    fontSize:30,
+    fontWeight:'bold',
+    textAlign:'center',
+  }
 });
--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,7 +1,7 @@
 const COLORS = {
     darkBlue: "#282F44",
-    Gold: "#E6AF2E",
-    baleGold: 	"#F5D061",
-    gray: "#ECECEC"
+    Gold:     "#E6AF2E",
+    baleGold: "#F5D061",
+    gray:     "#ECECEC"
 }
 export default COLORS;
