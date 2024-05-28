8b28b1ccb4d90d292c480ef5e32a1c3aeb893be2
BishoyTharwat <b.tharwat@penta-b.com>
2024-05-28 22:34:42 +0300

    Alerts on Login added

--- app/login/index.js
+++ app/login/index.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text, Platform } from 'react-native';
+import { StyleSheet, View, Text, Alert } from 'react-native';
 import { Modal, Portal, TextInput, Checkbox, Button, ActivityIndicator } from 'react-native-paper';
 import { useState } from 'react';
 import COLORS from '../../constants/colors';
@@ -9,9 +9,9 @@ import { router, Link } from 'expo-router';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Index() {
-  const [brand, setBrand] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [brand, setBrand] = useState(null);
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
   const [fingerprint, setFingerprint] = useState("");
   const [hidePassword, setHidePassword] = useState(true);
   const [checked, setChecked] = useState(false);
@@ -74,11 +74,15 @@ export default function Index() {
   const handleLogin = async () => {
     const isValid = await loginValidation();
     setIsLoginValid(isValid);
-    if (!isValid) {
-      console.log('Login invalid, stay on the same page.');
+    if (!isValid && !brand && !username) {
+      Alert.alert('تأكد من الاسم وكلمة المرور', '', [
+        {text: 'ok', onPress: () => console.log("invalid login")},
+      ])
     } else {
-      console.log('Login valid, navigate to dashboard.');
-      router.push('/dashboard');
+      Alert.alert('تم تسجيل الدخول بنجاح', '', [
+        {text: 'ok', onPress: () => router.push('/dashboard')},
+      ])
+      
     }
   };
 
